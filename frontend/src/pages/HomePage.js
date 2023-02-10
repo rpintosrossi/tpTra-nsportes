ea0deba51d5e48269e07fbbdcfc4426a5939140a
Franco Nicotra <fmnicotra@estudiantes.unsam.edu.ar>
2023-02-09 23:25:51 -0300

    home

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,25 +1,23 @@
-import react from 'react';
 import '../styles/components/pages/HomePage.css';
-
 
 const HomePage = (props) => {
     return (
         <main className="holder">
-            <div className="homeimg">
-                <img src="img/home/img01.jpg" alt="Avlon" />
+            <div>
+                <img src="img/home/img01.jpg" alt="avion" />
             </div>
             <div className="columnas">
-                <div className="bienvenidos">
-                    <h2>Blenvenidos</h2>
+                <section className="bienvenidos">
+                    <h2>Bienvenidos</h2>
                     <p>Transportes.Transportes.....</p>
-                </div>
-                <div className="testimonios">
+                </section>
+                <section className="testimonios">
                     <h2>Testimonios</h2>
                     <div className="testimonio">
                         <span className="cita">Simplemente Excelente</span>
                         <span className="autor">Juan Perez - zapatos.com</span>
                     </div>
-                </div>
+                </section>
             </div>
         </main >
     )
